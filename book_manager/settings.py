4e6dd8c8357acd2e09aa1d250957815b57e7409e
bogdand <dana.bogdan@flutteruki.com>
2026-07-23 20:55:13 +0300

    updated login views

--- book_manager/settings.py
+++ book_manager/settings.py
@@ -122,6 +122,12 @@ STATIC_URL = 'static/'
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
+LOGIN_URL = "login"
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
+
+
+
 
 
 # Email
